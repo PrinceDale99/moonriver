4bb0e38e06962cbdabc14025dd94245899cef9ad
AQUAMARINE - Prince Dale Limosnero <princedalelimosnero@gmail.com>
2025-08-28 00:59:06 +0000

    remove the text Moon River and the book logo on nav bar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,11 +6,7 @@ import { Button } from "./ui/button";
 export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-5xl items-center justify-between">
-        <Link href="/" className="flex items-center space-x-2">
-            <BookOpen className="h-6 w-6 text-primary" />
-            <span className="font-bold font-headline text-lg tracking-tight">Moon River</span>
-        </Link>
+      <div className="container flex h-14 max-w-5xl items-center justify-end">
         <ThemeToggle />
       </div>
     </header>
